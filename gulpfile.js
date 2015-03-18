61342803198eec840f1c27b389d86b8855f223f9
Marcio Augusto Guimarães <marcioaugustosg@gmail.com>
2015-03-18 18:18:32 -0300

    incluindo suporte ao sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,41 +7,51 @@
         browserify = require('gulp-browserify'),
         concat = require('gulp-concat'),
         wiredep = require('wiredep').stream,
-        runSequence = require('run-sequence');
+        runSequence = require('run-sequence'),
+        sass = require('gulp-sass');
 
     gulp.task('browserify', function () {
-        return gulp.src('src/javascript/main.js')
+        return gulp.src('./src/javascript/main.js')
             .pipe(browserify({
                 insertGlobals: true
             }))
-            .pipe(gulp.dest('.tmp/js'));
+            .pipe(gulp.dest('./.tmp/js'));
     });
 
     gulp.task('concat', function () {
-        return gulp.src('.tmp/js/*.js')
+        return gulp.src('./.tmp/js/*.js')
             .pipe(concat('main.js'))
-            .pipe(gulp.dest('web/js'));
+            .pipe(gulp.dest('./web/js'));
     });
 
     gulp.task('copy/background', function () {
-        return gulp.src('src/javascript/background.js')
-            .pipe(gulp.dest('web/js'));
+        return gulp.src('./src/javascript/background.js')
+            .pipe(gulp.dest('./web/js'));
     });
 
     gulp.task('wiredep', function () {
-        return gulp.src('web/index.html')
+        return gulp.src('./web/index.html')
             .pipe(wiredep())
-            .pipe(gulp.dest('web'));
+            .pipe(gulp.dest('./web'));
     });
+
+    gulp.task('sass', function () {
+        return gulp.src('./src/sass/styles.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('./web/stylesheets'));
+    });
+
+
 
     gulp.task('dev', function (done) {
         runSequence(
-            ['browserify', 'wiredep'],
+            ['browserify', 'wiredep', 'sass'],
             'concat',
             'copy/background',
             done
         );
     });
+
 
 }());
 
